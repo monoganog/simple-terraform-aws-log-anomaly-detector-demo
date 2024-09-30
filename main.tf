27573488cd69f027aa7519dacccf4337197540e3
Ben Monaghan <monoganog@gmail.com>
2024-09-30 20:25:23 +0100

    Upload tf code

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.69"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "awscc" {
+  region = var.aws_region
+}
+
+resource "awscc_logs_log_anomaly_detector" "my-anomaly-detector" {
+  log_group_arn_list    = [aws_cloudwatch_log_group.my-log-group.arn]
+  detector_name         = "example-detector"
+  evaluation_frequency  = "FIVE_MIN"  # Evaluate every 5 minutes
+}
+
+resource "aws_cloudwatch_log_group" "my-log-group" {
+  name = "log-group-example"
+  tags = {
+    Environment = "dev"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "aws_region" {
+  description = "AWS region for all resources."
+  value       = var.aws_region
+}
